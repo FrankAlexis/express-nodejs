6fedc529fe4203f272e7fa7406f7d991aa8bd6d1
Frank Alexis <frank@experimentality.co>
2024-04-11 08:07:21 -0500

    (feat): add new mongoclient config

--- src/database/config.js
+++ src/database/config.js
@@ -1,13 +1,27 @@
-const { connect } = require('mongoose')
+const { MongoClient } = require('mongoose')
 const MESSAGES = require('../helpers/messages')
 const log = require('../helpers/debug')
 
+const client = new MongoClient(process.env.DB_CONNECTION, {
+  serverApi: {
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
 const dbConnection = async () => {
   try {
-    //await connect(process.env.DB_CONNECTION)
+    await client.connect()
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    )
   } catch (error) {
     log(error)
-    //throw new Error(MESSAGES.dbConnectionError)
+    throw new Error(MESSAGES.dbConnectionError)
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close()
   }
 }
 
